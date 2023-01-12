57692df2f0b706fde4f85d29514cbed3d86a99dd
無常 <3142324836@qq.com>
2023-01-13 00:03:15 +0800

    Add Track

---
+++ src/audioCore/Track.cpp
@@ -0,0 +1,16 @@
+﻿#include "Track.h"
+
+Track::Track(const juce::AudioChannelSet& type)
+	: audioChannels(type) {
+	/** Set Channel Layout */
+	this->setChannelLayoutOfBus(true, 0, type);
+	this->setChannelLayoutOfBus(false, 0, type);
+
+	/** The main audio io node of the mixer */
+	this->audioInputNode = this->addNode(
+		std::make_unique<juce::AudioProcessorGraph::AudioGraphIOProcessor>(
+			juce::AudioProcessorGraph::AudioGraphIOProcessor::audioInputNode));
+	this->audioOutputNode = this->addNode(
+		std::make_unique<juce::AudioProcessorGraph::AudioGraphIOProcessor>(
+			juce::AudioProcessorGraph::AudioGraphIOProcessor::audioOutputNode));
+}
---
+++ src/audioCore/Track.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include <Defs.h>
+
+class Track final : public juce::AudioProcessorGraph {
+public:
+	Track() = delete;
+	Track(const juce::AudioChannelSet& type = juce::AudioChannelSet::stereo());
+
+private:
+	juce::AudioProcessorGraph::Node::Ptr audioInputNode, audioOutputNode;
+	juce::AudioChannelSet audioChannels;
+};
