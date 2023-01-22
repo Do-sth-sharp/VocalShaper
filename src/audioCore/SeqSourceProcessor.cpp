1a231712b88930aeaea687b9ac2a663fbf4abcfe
無常 <3142324836@qq.com>
2023-01-22 23:19:35 +0800

    Add SeqSourceProcessor

---
+++ src/audioCore/SeqSourceProcessor.cpp
@@ -0,0 +1,8 @@
+﻿#include "SeqSourceProcessor.h"
+
+SeqSourceProcessor::SeqSourceProcessor(const juce::AudioChannelSet& type)
+	: audioChannels(type) {
+	/** Set Channel Layout */
+	this->setChannelLayoutOfBus(true, 0, type);
+	this->setChannelLayoutOfBus(false, 0, type);
+}
---
+++ src/audioCore/SeqSourceProcessor.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+#include <Defs.h>
+
+class SeqSourceProcessor final : public juce::AudioProcessor {
+public:
+	SeqSourceProcessor() = delete;
+	SeqSourceProcessor(const juce::AudioChannelSet& type = juce::AudioChannelSet::stereo());
+
+public:
+	const juce::String getName() const override { return "SeqSource"; };
+
+	void prepareToPlay(double sampleRate, int maximumExpectedSamplesPerBlock) override {};
+	void releaseResources() override {};
+	void processBlock(juce::AudioBuffer< float >& buffer, juce::MidiBuffer& midiMessages) override {};
+
+	bool acceptsMidi() const override { return true; };
+	bool producesMidi() const override { return true; };
+
+	juce::AudioProcessorEditor* createEditor() override { return nullptr; };
+	bool hasEditor() const override { return false; };
+
+	int getNumPrograms() override { return 0; };
+	int getCurrentProgram() override { return 0; };
+	void setCurrentProgram(int index) override {};
+	const juce::String getProgramName(int index) override { return juce::String(); };
+	void changeProgramName(int index, const juce::String& newName) override {};
+
+	void getStateInformation(juce::MemoryBlock& destData) override {};
+	void setStateInformation(const void* data, int sizeInBytes) override {};
+
+	double getTailLengthSeconds() const override { return 0; };
+
+private:
+	juce::AudioChannelSet audioChannels;
+
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SeqSourceProcessor)
+};
