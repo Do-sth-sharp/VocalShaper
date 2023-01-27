abfe715ca688414c17938b6d6c88a7bd1594cdee
無常 <3142324836@qq.com>
2023-01-27 22:16:20 +0800

    Update AudioCore

--- src/audioCore/AudioCore.cpp
+++ src/audioCore/AudioCore.cpp
@@ -9,9 +9,57 @@ AudioCore::AudioCore() {
 	/** Main Audio Graph Of The Audio Core */
 	this->mainAudioGraph = std::make_unique<juce::AudioProcessorGraph>();
 
+	/** Main Graph Player */
+	this->mainGraphPlayer = std::make_unique<juce::AudioProcessorPlayer>();
+
+	/** The Main Audio IO Node Of The Audio Core */
+	this->audioInputNode = this->mainAudioGraph->addNode(
+		std::make_unique<juce::AudioProcessorGraph::AudioGraphIOProcessor>(
+			juce::AudioProcessorGraph::AudioGraphIOProcessor::audioInputNode));
+	this->audioOutputNode = this->mainAudioGraph->addNode(
+		std::make_unique<juce::AudioProcessorGraph::AudioGraphIOProcessor>(
+			juce::AudioProcessorGraph::AudioGraphIOProcessor::audioOutputNode));
+
+	/** The Main MIDI Input Node Of The Audio Core */
+	this->midiInputNode = this->mainAudioGraph->addNode(
+		std::make_unique<juce::AudioProcessorGraph::AudioGraphIOProcessor>(
+			juce::AudioProcessorGraph::AudioGraphIOProcessor::midiInputNode));
+
 	/** Add Sequencer And Mixer To Audio Graph */
 	this->sequencer = this->mainAudioGraph->addNode(std::make_unique<Sequencer>());
 	this->mixer = this->mainAudioGraph->addNode(std::make_unique<Mixer>());
+
+	/** Add MIDI Connection */
+	this->mainAudioGraph->addConnection(
+		{ {this->midiInputNode->nodeID, juce::AudioProcessorGraph::midiChannelIndex},
+		{this->sequencer->nodeID, juce::AudioProcessorGraph::midiChannelIndex} });
+	this->mainAudioGraph->addConnection(
+		{ {this->midiInputNode->nodeID, juce::AudioProcessorGraph::midiChannelIndex},
+		{this->mixer->nodeID, juce::AudioProcessorGraph::midiChannelIndex} });
+	this->mainAudioGraph->addConnection(
+		{ {this->sequencer->nodeID, juce::AudioProcessorGraph::midiChannelIndex},
+		{this->mixer->nodeID, juce::AudioProcessorGraph::midiChannelIndex} });
+
+	/** Init Audio Device */
+	this->initAudioDevice();
+}
+
+void AudioCore::initAudioDevice() {
+	/** Init With Default Device */
+	this->audioDeviceManager->initialiseWithDefaultDevices(0, 2);
+
+	/** Add Main Graph To Main Player */
+	this->mainGraphPlayer->setProcessor(this->mainAudioGraph.get());
+
+	/** Add Player To Default Device */
+	this->audioDeviceManager->addAudioCallback(this->mainGraphPlayer.get());
+
+	/** Update Audio Buses */
+	this->updateAudioBuses();
+}
+
+void AudioCore::updateAudioBuses() {
+
 }
 
 AudioCore* AudioCore::getInstance() {
--- src/audioCore/AudioCore.h
+++ src/audioCore/AudioCore.h
@@ -9,8 +9,15 @@ public:
 private:
 	std::unique_ptr<juce::AudioDeviceManager> audioDeviceManager = nullptr;
 	std::unique_ptr<juce::AudioProcessorGraph> mainAudioGraph = nullptr;
+	std::unique_ptr<juce::AudioProcessorPlayer> mainGraphPlayer = nullptr;
+
 	juce::AudioProcessorGraph::Node::Ptr sequencer;
 	juce::AudioProcessorGraph::Node::Ptr mixer;
+	juce::AudioProcessorGraph::Node::Ptr audioInputNode, audioOutputNode;
+	juce::AudioProcessorGraph::Node::Ptr midiInputNode;
+
+	void initAudioDevice();
+	void updateAudioBuses();
 
 private:
 	static AudioCore* getInstance();
