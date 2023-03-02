fffe7d2e4fcc78f41a5fc9c6dc1446071052a1fa
無常 <3142324836@qq.com>
2023-03-02 15:10:53 +0800

    Update main.lua comments

--- app/main.lua
+++ app/main.lua
@@ -5,9 +5,11 @@ require "luce.FlowWindow"
 require "luce.ResizableWindow"
 
 luce.initialise = function(commandLineParameters)
+	-- Load Config And Theme
 	conf = require("ui.Config")
 	ui = require("ui.themes." .. conf.theme)
 
+	-- Set UI Style
 	require("ui.FlowStyle")
 	updateFlowColor()
 	require("ui.MenuStyle")
@@ -15,9 +17,11 @@ luce.initialise = function(commandLineParameters)
 	require("ui.AlertStyle")
 	updateAlertColor()
 
+	-- Set Window Background Color
 	luce.Desktop.getInstance():getDefaultLookAndFeel():setColour(
 			luce.ResizableWindow.ColourIds.backgroundColourId, ui.color.themeColorB2)
 
+	-- Set FlowUI Button ICON
 	luce.FlowStyle.setButtonLeftIcon("./RemixIcon/Design/layout-left-2-line.svg")
 	luce.FlowStyle.setButtonRightIcon("./RemixIcon/Design/layout-right-2-line.svg")
 	luce.FlowStyle.setButtonTopIcon("./RemixIcon/Design/layout-top-2-line.svg")
@@ -28,13 +32,18 @@ luce.initialise = function(commandLineParameters)
 	luce.FlowStyle.setButtonAdsorbTopIcon("./RemixIcon/Design/layout-top-2-line.svg")
 	luce.FlowStyle.setButtonAdsorbBottomIcon("./RemixIcon/Design/layout-bottom-2-line.svg")
 
+	-- Set Flow Window ICON
 	luce.FlowWindow.setIcon("./rc/logo.png")
+	-- Flow Window Use OpenGL Renderer
 	luce.FlowWindow.setOpenGL(true)
 
+	-- Load UI Translate
 	luce.LocalisedStrings.loadData("./translates/" .. conf.language .. "/")
+	-- Set UI Font
 	luce.Desktop.getInstance():getDefaultLookAndFeel():setDefaultSansSerifTypeface(
 		"./fonts/" .. conf.font .. ".ttf")
 
+	-- Create Flow Components
 	startMenu = luce.FlowComponent.new("StartMenu")
 	toolBar = luce.FlowComponent.new("ToolBar")
 	resourceView = luce.FlowComponent.new("Resource")
@@ -43,9 +52,11 @@ luce.initialise = function(commandLineParameters)
 	audioDebugger = require("ui.AudioDebugger")
 	midiDebugger = require("ui.MIDIDebugger")
 
+	-- Auto Layout Flow Components
 	luce.FlowWindow.layout("./layouts/" .. conf.layout .. ".json",
 		toolBar, resourceView, patternView, trackView, audioDebugger, midiDebugger)
 
+	-- Set Main Window Full Screen
 	local windowNum = luce.FlowWindow.getWindowNum()
 	if windowNum > 0 then
 		local mainWindow = luce.FlowWindow.getWindow(0)
@@ -54,8 +65,10 @@ luce.initialise = function(commandLineParameters)
 end
 
 luce.shutdown = function()
+	-- Shutdown FlowUI At Application Shutdown To Avoid Memory Leak
 	luce.FlowWindow.shutdown()
 end
 
 luce.anotherInstanceStarted = function(commandLine)
+	-- TODO Another App Instance Started
 end
