1074e5d97df3af9d0d347b7e269e849817b46926
無常 <3142324836@qq.com>
2023-05-16 08:55:58 +0800

    Update script comment

--- test/testPlugin.lua
+++ test/testPlugin.lua
@@ -1,17 +1,22 @@
+-- Create A Stereo Track
 AC.addMixerTrack(-1, -1);
 AC.addMixerTrackInputFromDevice(0, 0, 0);
 AC.addMixerTrackInputFromDevice(0, 0, 1);
 AC.addMixerTrackOutput(0, 0, 0);
 AC.addMixerTrackOutput(0, 1, 1);
 
+-- Set Gain And Add Effect Plugin
 AC.setMixerTrackGain(0, 5);
 AC.addMixerPlugin(0, 0, "VST3-Experiverb-31f1abe0-a118c70b");
 AC.addMixerPlugin(0, 1, "VST3-Clone-deb2f263-f74aeabb");
 
+-- Remove Effect Plugin
 AC.removeMixerPlugin(0, 0);
 
+-- Effect Plugin Window
 AC.setMixerPluginWindow(0, 0, true);
 AC.setMixerPluginWindow(0, 0, false);
 
+-- Effect Plugin Bypass
 AC.setMixerPluginBypass(0, 0, true);
 AC.setMixerPluginBypass(0, 0, false);
