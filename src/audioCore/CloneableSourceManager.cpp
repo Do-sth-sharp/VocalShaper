92f5ab5968f4b18d83b2879739ecaa0f2ef8a750
無常 <3142324836@qq.com>
2023-05-25 14:51:59 +0800

    Update CloneableSourceManager

--- src/audioCore/CloneableSourceManager.cpp
+++ src/audioCore/CloneableSourceManager.cpp
@@ -1,5 +1,25 @@
 #include "CloneableSourceManager.h"
 
+void CloneableSourceManager::addSource(std::unique_ptr<CloneableSource> src) {
+	this->sourceList.add(std::move(src));
+}
+
+void CloneableSourceManager::removeSource(CloneableSource* src) {
+	this->sourceList.removeObject(src, true);
+}
+
+void CloneableSourceManager::removeSource(int index) {
+	this->sourceList.remove(index, true);
+}
+
+CloneableSource* CloneableSourceManager::getSource(int index) const {
+	return this->sourceList[index];
+}
+
+int CloneableSourceManager::getSourceNum() const {
+	return this->sourceList.size();
+}
+
 CloneableSourceManager* CloneableSourceManager::getInstance() {
 	return CloneableSourceManager::instance
 		? CloneableSourceManager::instance
--- src/audioCore/CloneableSourceManager.h
+++ src/audioCore/CloneableSourceManager.h
@@ -1,11 +1,21 @@
 #pragma once
 
 #include <JuceHeader.h>
+#include "CloneableSource.h"
 
 class CloneableSourceManager final : public juce::DeletedAtShutdown {
 public:
 	CloneableSourceManager() = default;
 	~CloneableSourceManager() override = default;
+
+	void addSource(std::unique_ptr<CloneableSource> src);
+	void removeSource(CloneableSource* src);
+	void removeSource(int index);
+	CloneableSource* getSource(int index) const;
+	int getSourceNum() const;
+
+private:
+	juce::OwnedArray<CloneableSource> sourceList;
 
 public:
 	static CloneableSourceManager* getInstance();
