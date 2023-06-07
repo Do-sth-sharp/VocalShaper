dcb37fad762b2ebed082341a7de6283ab3485b79
無常 <3142324836@qq.com>
2023-06-07 23:16:31 +0800

    Update PlayPosition

--- src/audioCore/PlayPosition.cpp
+++ src/audioCore/PlayPosition.cpp
@@ -1,8 +1,41 @@
 #include "PlayPosition.h"
 
+#include <chrono>
+
 juce::Optional<juce::AudioPlayHead::PositionInfo> PlayPosition::getPosition() const {
-	/** TODO Get Play Position */
-	return juce::Optional<juce::AudioPlayHead::PositionInfo>{};
+	juce::ScopedReadLock locker(this->lock);
+
+	/** Get System Clock Nanoseconds */
+	std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+	std::chrono::nanoseconds ns
+		= std::chrono::duration_cast<std::chrono::nanoseconds>(now.time_since_epoch());
+	this->position.setHostTimeNs((uint64_t)ns.count());
+
+	return juce::makeOptional(this->position);
+}
+
+bool PlayPosition::canControlTransport() {
+	//return juce::MessageManager::existsAndIsCurrentThread();
+	return true;
+}
+
+void PlayPosition::transportPlay(bool shouldStartPlaying) {
+	juce::ScopedWriteLock locker(this->lock);
+	this->position.setIsPlaying(shouldStartPlaying);
+}
+
+void PlayPosition::transportRecord(bool shouldStartRecording) {
+	juce::ScopedWriteLock locker(this->lock);
+	this->position.setIsRecording(shouldStartRecording);
+}
+
+void PlayPosition::transportRewind() {
+	juce::ScopedWriteLock locker(this->lock);
+	this->position.setTimeInSamples(0);
+	this->position.setTimeInSeconds(0);
+	this->position.setBarCount(0);
+	this->position.setPpqPositionOfLastBarStart(0);
+	this->position.setPpqPosition(0);
 }
 
 PlayPosition* PlayPosition::getInstance() {
--- src/audioCore/PlayPosition.h
+++ src/audioCore/PlayPosition.h
@@ -10,6 +10,15 @@ public:
 
 	juce::Optional<juce::AudioPlayHead::PositionInfo> getPosition() const override;
 
+	bool canControlTransport() override;
+	void transportPlay(bool shouldStartPlaying) override;
+	void transportRecord(bool shouldStartRecording) override;
+	void transportRewind() override;
+
+private:
+	mutable juce::AudioPlayHead::PositionInfo position;
+	juce::ReadWriteLock lock;
+
 public:
 	static PlayPosition* getInstance();
 	static void releaseInstance();
