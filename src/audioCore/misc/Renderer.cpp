375c9fc678323e5ad542a7e05c218bbcaebb0ac8
無常 <3142324836@qq.com>
2023-09-17 23:50:57 +0800

    Update Renderer

--- src/audioCore/misc/Renderer.cpp
+++ src/audioCore/misc/Renderer.cpp
@@ -1,5 +1,18 @@
 #include "Renderer.h"
 
+void Renderer::setRendering(bool rendering) {
+	this->rendering = rendering;
+}
+
+bool Renderer::getRendering() const {
+	return this->rendering;
+}
+
+void Renderer::writeData(int trackNum, const juce::AudioBuffer<float>& buffer, int offset) {
+	if (!this->rendering) { return; }
+	/** TODO Write To Audio Collector */
+}
+
 Renderer* Renderer::getInstance() {
 	return Renderer::instance
 		? Renderer::instance : (Renderer::instance = new Renderer());
--- src/audioCore/misc/Renderer.h
+++ src/audioCore/misc/Renderer.h
@@ -6,6 +6,19 @@ class Renderer final : private juce::DeletedAtShutdown {
 public:
 	Renderer() = default;
 
+	void setRendering(bool rendering);
+	bool getRendering() const;
+
+	/** TODO Prepare To Render */
+	/** TODO Write Audo File */
+
+private:
+	friend class Track;
+	void writeData(int trackNum, const juce::AudioBuffer<float>& buffer, int offset);
+
+private:
+	std::atomic_bool rendering = false;
+
 public:
 	static Renderer* getInstance();
 	static void releaseInstance();
