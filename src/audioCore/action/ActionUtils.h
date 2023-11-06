f71e6a4013cdb5055940c683b7500d7008dca1ef
WuChang <3142324836@qq.com>
2023-11-06 23:53:23 +0800

    Add ActionUtils

---
+++ src/audioCore/action/ActionUtils.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include <JuceHeader.h>
+
+#include "../misc/Renderer.h"
+#include "../source/AudioIOList.h"
+#include "../plugin/PluginLoader.h"
+
+#define ACTION_CHECK_RENDERING(s) \
+	do { \
+		if(Renderer::getInstance()->getRendering()) { \
+			this->output(s); \
+			return false; \
+		} \
+	} while(false)
+
+#define ACTION_CHECK_SOURCE_IO_RUNNING(s) \
+	do { \
+		if(AudioIOList::getInstance()->isThreadRunning()) { \
+			this->output(s); \
+			return false; \
+		} \
+	} while(false)
+
+#define ACTION_CHECK_PLUGIN_LOADING(s) \
+	do { \
+		if(PluginLoader::getInstance()->isRunning()) { \
+			this->output(s); \
+			return false; \
+		} \
+	} while(false)
