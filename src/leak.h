594ad5ad7e4bbc4bc082e9e4cf57d0b14700076b
WuChang <3142324836@qq.com>
2023-12-01 01:02:08 +0800

    Add newd

---
+++ src/leak.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+#include <JuceHeader.h>
+
+#ifdef _DEBUG && JUCE_MSVC && !JUCE_CHECK_MEMORY_LEAKS
+#define newd new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+#else
+#define newd new
+#endif
