252e1050d0386d0121d90cf57770d1342d0a1cf5
WuChang <3142324836@qq.com>
2023-12-01 00:51:46 +0800

    Removed JUCE leak detector

---
+++ src/leak.cpp
@@ -0,0 +1,13 @@
+﻿#include <JuceHeader.h>
+
+#if JUCE_MSVC && !JUCE_CHECK_MEMORY_LEAKS
+struct DebugFlagsInitialiser
+{
+    DebugFlagsInitialiser()
+    {
+        _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+    }
+};
+
+static DebugFlagsInitialiser debugFlagsInitialiser;
+#endif
