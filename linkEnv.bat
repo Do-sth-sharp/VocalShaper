1c0b950eb45d3936144911933a5bdc766aec48a6
WuChang <3142324836@qq.com>
2023-12-03 23:33:13 +0800

    Add licenses

--- linkEnv.bat
+++ linkEnv.bat
@@ -1,1 +1,32 @@
-﻿mklink /H /J .\app\RemixIcon .\RemixIcon\icons
+﻿%RemixIcon%
+mklink /H /J .\app\RemixIcon .\RemixIcon\icons
+
+%Licenses%
+mklink /H .\app\licenses\VocalShaper.txt .\LICENSE
+mklink /H .\app\licenses\JUCE.md .\JUCE\LICENSE.md
+mklink /H .\app\licenses\FlowUI.txt .\FlowUI\LICENSE
+mklink /H .\app\licenses\libDMDA.txt .\libDMDA\LICENSE
+mklink /H .\app\licenses\libMackieControl.txt .\libMackieControl\LICENSE
+mklink /H .\app\licenses\SynthEngineDemo.txt .\SynthEngineDemo\LICENSE
+mklink /H .\app\licenses\VocalShaperProject4.txt .\VocalShaperProject4\LICENSE
+mklink /H .\app\licenses\Protocol-Buffers.txt .\VocalShaperProject4\protobuf\LICENSE
+mklink /H .\app\licenses\Abseil.txt .\VocalShaperProject4\protobuf\third_party\abseil-cpp\LICENSE
+mklink /H .\app\licenses\JsonCpp.txt .\VocalShaperProject4\protobuf\third_party\jsoncpp\LICENSE
+mklink /H .\app\licenses\GoogleTest.txt .\VocalShaperProject4\protobuf\third_party\googletest\LICENSE
+mklink /H .\app\licenses\utf8_range.txt .\VocalShaperProject4\protobuf\third_party\utf8_range\LICENSE
+mklink /H .\app\licenses\lunit.txt .\VocalShaperProject4\protobuf\third_party\lunit\LICENSE
+mklink /H .\app\licenses\RemixIcon.txt .\RemixIcon\License
+mklink /H .\app\licenses\cmake-lame.txt .\cmake-lame\LICENSE
+mklink /H .\app\licenses\lame.txt .\cmake-lame\lame-3.100\LICENSE
+mklink /H .\app\licenses\lua.txt .\LICENSE-Lua.txt
+mklink /H .\app\licenses\ASIO-SDK.txt .\LICENSE-ASIOSDK.txt
+mklink /H .\app\licenses\VST3-SDK.txt .\vst3sdk\LICENSE.txt
+mklink /H .\app\licenses\VST2-SDK.txt .\LICENSE-VST2SDK.txt
+mklink /H .\app\licenses\LILV.txt .\JUCE\modules\juce_audio_processors\format_types\LV2_SDK\lilv\COPYING
+mklink /H .\app\licenses\LV2.txt .\JUCE\modules\juce_audio_processors\format_types\LV2_SDK\lv2\COPYING
+mklink /H .\app\licenses\Serd.txt .\JUCE\modules\juce_audio_processors\format_types\LV2_SDK\serd\COPYING
+mklink /H .\app\licenses\Sord.txt .\JUCE\modules\juce_audio_processors\format_types\LV2_SDK\sord\COPYING
+mklink /H .\app\licenses\Sratom.txt .\JUCE\modules\juce_audio_processors\format_types\LV2_SDK\sratom\COPYING
+mklink /H .\app\licenses\AudioUnitSDK.txt .\JUCE\modules\juce_audio_plugin_client\AU\AudioUnitSDK\LICENSE.txt
+mklink /H .\app\licenses\pnglib.txt .\JUCE\modules\juce_graphics\image_formats\pnglib\LICENSE
+mklink /H .\app\licenses\oboe.txt .\JUCE\modules\juce_audio_devices\native\oboe\LICENSE
