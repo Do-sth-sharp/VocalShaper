fd88e6685cebe2278a26dc1226a32fd0f552539c
WuChang <3142324836@qq.com>
2023-12-10 23:21:13 +0800

    Add SysStatus

---
+++ src/ui/misc/SysStatus.cpp
@@ -0,0 +1,70 @@
+﻿#include "SysStatus.h"
+
+#include <sigar.h>
+
+double SysStatus::getCPUUsage() {
+	/** Open Sigar */
+	sigar_t* pSigar = nullptr;
+	sigar_open(&pSigar);
+	if (!pSigar) { return 0; }
+
+	/** Get CPU List */
+	sigar_cpu_list_t cpuList = {};
+	sigar_cpu_list_get(pSigar, &cpuList);
+
+	/** Get CPU Prec */
+	double total = 0;
+	int cpuNum = cpuList.number;
+	for (int i = 0; i < cpuNum; i++) {
+		sigar_cpu_t* cpu = &(cpuList.data[i]);
+		total += ((cpu->total - cpu->idle) / (double)cpu->total);
+	}
+	total /= cpuNum;
+
+	/** Clear */
+	sigar_cpu_list_destroy(pSigar, &cpuList);
+	sigar_close(pSigar);
+
+	return total;
+}
+
+double SysStatus::getMemUsage() {
+	/** Open Sigar */
+	sigar_t* pSigar = nullptr;
+	sigar_open(&pSigar);
+	if (!pSigar) { return 0; }
+
+	/** Get Mem */
+	sigar_mem_t mem = {};
+	sigar_mem_get(pSigar, &mem);
+
+	/** Get Result */
+	double result = mem.used_percent;
+
+	/** Clear */
+	sigar_close(pSigar);
+
+	return result;
+}
+
+uint64_t SysStatus::getProcMemUsage() {
+	/** Open Sigar */
+	sigar_t* pSigar = nullptr;
+	sigar_open(&pSigar);
+	if (!pSigar) { return 0; }
+
+	/** Get PID */
+	sigar_pid_t pid = sigar_pid_get(pSigar);
+
+	/** Get Mem */
+	sigar_proc_mem_t mem = {};
+	sigar_proc_mem_get(pSigar, pid, &mem);
+
+	/** Get Result */
+	uint64_t result = mem.size;
+
+	/** Clear */
+	sigar_close(pSigar);
+
+	return result;
+}
---
+++ src/ui/misc/SysStatus.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+#include <JuceHeader.h>
+
+class SysStatus final {
+public:
+	static double getCPUUsage();
+	static double getMemUsage();
+	static uint64_t getProcMemUsage();
+};
