84bb701b0eb4f561eb7cf51adfccbfbed52d6120
WuChang <3142324836@qq.com>
2024-02-27 00:29:48 +0800

    Add SourceIO

---
+++ src/audioCore/misc/SourceIO.cpp
@@ -0,0 +1,77 @@
+﻿#include "SourceIO.h"
+#include "../Utils.h"
+
+SourceIO::SourceIO()
+	: Thread("Source IO"),
+	audioFormatsIn(SourceIO::trimFormat(utils::getAudioFormatsSupported(false))),
+	midiFormatsIn(SourceIO::trimFormat(utils::getMidiFormatsSupported(false))),
+	audioFormatsOut(SourceIO::trimFormat(utils::getAudioFormatsSupported(true))),
+	midiFormatsOut(SourceIO::trimFormat(utils::getMidiFormatsSupported(true))) {}
+
+SourceIO::~SourceIO() {
+	this->stopThread(30000);
+}
+
+void SourceIO::addTask(const Task& task) {
+	juce::GenericScopedLock locker(this->lock);
+	this->list.push(task);
+}
+
+void SourceIO::run() {
+	while (!this->threadShouldExit()) {
+		/** Get Next Task */
+		Task task;
+		{
+			juce::GenericScopedLock locker(this->lock);
+
+			/** Check Empty */
+			if (this->list.empty()) { break; }
+
+			/** Dequque Task */
+			task = this->list.front();
+			this->list.pop();
+		}
+
+		/** Process Task */
+		{
+			/** Check Source Type */
+			auto& [type, ptr, path] = task;
+			if (!ptr) { continue; }
+
+			juce::File file = utils::getProjectDir().getChildFile(path);
+			juce::String extension = file.getFileExtension();
+			
+			auto& audioTypes = ((type == TaskType::Read) ? this->audioFormatsIn : this->audioFormatsOut);
+			auto& midiTypes = ((type == TaskType::Read) ? this->midiFormatsIn : this->midiFormatsOut);
+
+			if (audioTypes.contains(extension)) {
+				/** TODO */
+			}
+			else if (midiTypes.contains(extension)) {
+				/** TODO */
+			}
+		}
+	}
+}
+
+const juce::StringArray SourceIO::trimFormat(const juce::StringArray& list) {
+	juce::StringArray result;
+	for (auto& s : list) {
+		result.add(s.trimCharactersAtStart("*"));
+	}
+	return result;
+}
+
+SourceIO* SourceIO::getInstance() {
+	return SourceIO::instance
+		? SourceIO::instance : (SourceIO::instance = new SourceIO);
+}
+
+void SourceIO::releaseInstance() {
+	if (SourceIO::instance) {
+		delete SourceIO::instance;
+		SourceIO::instance = nullptr;
+	}
+}
+
+SourceIO* SourceIO::instance = nullptr;
---
+++ src/audioCore/misc/SourceIO.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include <JuceHeader.h>
+#include "../graph/SeqSourceProcessor.h"
+
+class SourceIO final : public juce::Thread,
+	private juce::DeletedAtShutdown {
+public:
+	SourceIO();
+	~SourceIO();
+
+	enum class TaskType {
+		Read, Write
+	};
+	/** Type, SeqPtr, Path */
+	using Task = std::tuple<TaskType, SeqSourceProcessor::SafePointer, juce::String>;
+	void addTask(const Task& task);
+
+protected:
+	void run() override;
+
+private:
+	const juce::StringArray audioFormatsIn, midiFormatsIn;
+	const juce::StringArray audioFormatsOut, midiFormatsOut;
+
+	juce::CriticalSection lock;
+	std::queue<Task> list;
+
+	static const juce::StringArray trimFormat(const juce::StringArray& list);
+
+public:
+	static SourceIO* getInstance();
+	static void releaseInstance();
+
+private:
+	static SourceIO* instance;
+
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SourceIO)
+};
