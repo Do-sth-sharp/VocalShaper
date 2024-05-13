de3c5a2b62c0b3eb6bd262b224d36a1f8c2bd653
WuChang <3142324836@qq.com>
2024-05-13 17:30:52 +0800

    Add AudioExtractor

---
+++ src/ui/misc/AudioExtractor.cpp
@@ -0,0 +1,44 @@
+﻿#include "AudioExtractor.h"
+#include "MainThreadPool.h"
+
+void AudioExtractor::extractAsync(
+	const AudioData& data, uint64_t pointNum,
+	const Callback& callback) {
+	auto job = [data, pointNum, callback] {
+		AudioExtractor::extractInternal(
+			std::get<1>(data), pointNum, callback);
+		};
+	MainThreadPool::getInstance()->runJob(job);
+}
+
+void AudioExtractor::extractInternal(
+	const juce::AudioSampleBuffer& data, uint64_t pointNum,
+	const Callback& callback) {
+	/** Prepare Data */
+	int sampleNum = data.getNumSamples();
+	double clipSize = sampleNum / (double)pointNum;
+
+	/** Result */
+	Result result;
+
+	/** Get Each Track */
+	for (int i = 0; i < data.getNumChannels(); i++) {
+		auto dataPtr = data.getReadPointer(i);
+		juce::MemoryBlock channelResult(pointNum * sizeof(float) * 2);
+
+		/** Get Clip Data */
+		for (int j = 0; j < pointNum; j++) {
+			if (clipSize >= 1) {
+				/** TODO Extract */
+			}
+			else {
+				/** TODO Interpolation */
+			}
+		}
+
+		result.add(std::move(channelResult));
+	}
+
+	/** Callback */
+	juce::MessageManager::callAsync(std::bind(callback, result));
+}
---
+++ src/ui/misc/AudioExtractor.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include <JuceHeader.h>
+
+class AudioExtractor final {
+	AudioExtractor() = delete;
+
+public:
+	using AudioData = std::tuple<double, juce::AudioSampleBuffer>;
+	using Result = juce::Array<juce::MemoryBlock>;
+	using Callback = std::function<void(const Result&)>;
+
+	static void extractAsync(
+		const AudioData& data, uint64_t pointNum,
+		const Callback& callback);
+
+private:
+	static void extractInternal(
+		const juce::AudioSampleBuffer& data, uint64_t pointNum,
+		const Callback& callback);
+};
