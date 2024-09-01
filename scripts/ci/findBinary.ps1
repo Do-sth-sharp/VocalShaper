63c24e305a52958c5979709e56980298619887fb
無常 <3142324836@qq.com>
2024-09-02 00:14:12 +0800

    Update CI vcpkg cache

---
+++ scripts/ci/findBinary.ps1
@@ -0,0 +1,22 @@
+﻿param (
+    [string]$DirectoryPath
+)
+
+# Check if the directory path is provided and exists
+if (-not (Test-Path -Path $DirectoryPath)) {
+    exit
+}
+
+# Get all .exe .dll and .vst3 files recursively from the specified directory
+$files = Get-ChildItem -Path $DirectoryPath -Recurse -Include *.exe, *.dll, *.vst3
+
+# Format the output
+$output = $files | ForEach-Object {
+    '"' + $_.FullName + '"'
+} 
+
+# Join the file names with a comma separator
+$joinedOutput = $output -join ","
+
+# Print the output
+Write-Output $joinedOutput
---
+++ scripts/ci/prePacking.ps1
@@ -0,0 +1,43 @@
+﻿param (
+    [string]$DirectoryPath
+)
+
+# Check if the directory path is provided and exists
+if (-not (Test-Path -Path $DirectoryPath)) {
+    exit
+}
+
+# Remove audio plugin list
+$AUDIO_PATH = $DirectoryPath + "/data/audio"
+Remove-Item $($AUDIO_PATH + "/blackPlugins.txt")
+Remove-Item $($AUDIO_PATH + "/pluginPaths.txt")
+Remove-Item $($AUDIO_PATH + "/plugins.xml")
+
+$DEAD_PLUGINS_PATH = $AUDIO_PATH + "/deadPlugins"
+if ((Test-Path $DEAD_PLUGINS_PATH) -eq "True") {
+	Remove-Item $DEAD_PLUGINS_PATH -Recurse
+}
+
+# Remove build temp
+$REMOVE_PATH = $DirectoryPath + "/*"
+
+Remove-Item $REMOVE_PATH -Include "*.vshl.dmp"
+Remove-Item $REMOVE_PATH -Include "*.vsml.dmp"
+Remove-Item $REMOVE_PATH -Include "*.vsll.dmp"
+
+Remove-Item $REMOVE_PATH -Include "*.iobj"
+Remove-Item $REMOVE_PATH -Include "*.ipdb"
+Remove-Item $REMOVE_PATH -Include "*.exp"
+Remove-Item $REMOVE_PATH -Include "*.ilk"
+Remove-Item $REMOVE_PATH -Include "*.lib"
+Remove-Item $REMOVE_PATH -Include "*.a"
+
+# Remove engine build temp
+$ENGINE_REMOVE_PATH = $DirectoryPath + "/synth-engines/*"
+
+Remove-Item $ENGINE_REMOVE_PATH -Include "*.iobj"
+Remove-Item $ENGINE_REMOVE_PATH -Include "*.ipdb"
+Remove-Item $ENGINE_REMOVE_PATH -Include "*.exp"
+Remove-Item $ENGINE_REMOVE_PATH -Include "*.ilk"
+Remove-Item $ENGINE_REMOVE_PATH -Include "*.lib"
+Remove-Item $ENGINE_REMOVE_PATH -Include "*.a"
