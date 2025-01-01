db37e5f8130e2dd1a637cc325168fb248811dd6f
無常 <3142324836@qq.com>
2025-01-02 01:00:07 +0800

    Update core callbacks

--- src/ui/misc/CoreCallbacks.cpp
+++ src/ui/misc/CoreCallbacks.cpp
@@ -4,337 +4,117 @@
 CoreCallbacks::CoreCallbacks() {
 	UICallbackAPI<const juce::String&, const juce::String&>::set(UICallbackType::ErrorAlert,
 		[](const juce::String& title, const juce::String& mes) {
-			CoreCallbacks::getInstance()->invokeError(title, mes);
+			CoreCallbackAPI<const juce::String&, const juce::String&>::invoke(UICallbackType::ErrorAlert, title, mes);
 		});
 	UICallbackAPI<bool>::set(UICallbackType::PlayStateChanged,
 		[](bool status) {
-			CoreCallbacks::getInstance()->invokePlayingStatus(status);
+			CoreCallbackAPI<bool>::invoke(UICallbackType::PlayStateChanged, status);
 		});
 	UICallbackAPI<bool>::set(UICallbackType::RecordStateChanged,
 		[](bool status) {
-			CoreCallbacks::getInstance()->invokeRecordingStatus(status);
+			CoreCallbackAPI<bool>::invoke(UICallbackType::RecordStateChanged, status);
 		});
 	UICallbackAPI<const juce::String&>::set(UICallbackType::ErrorMessage,
 		[](const juce::String& mes) {
-			CoreCallbacks::getInstance()->invokeErrorMes(mes);
+			CoreCallbackAPI<const juce::String&>::invoke(UICallbackType::ErrorMessage, mes);
 		});
 	UICallbackAPI<bool>::set(UICallbackType::PluginSearchStateChanged,
 		[](bool state) {
-			CoreCallbacks::getInstance()->invokeSearchPlugin(state);
-		});
-	UICallbackAPI<int>::set(UICallbackType::SourceChanged,
-		[](int index) {
-			CoreCallbacks::getInstance()->invokeSourceChanged(index);
-		});
-	UICallbackAPI<int>::set(UICallbackType::InstrChanged,
-		[](int index) {
-			CoreCallbacks::getInstance()->invokeInstrChanged(index);
-		});
-	UICallbackAPI<int>::set(UICallbackType::TrackChanged,
-		[](int index) {
-			CoreCallbacks::getInstance()->invokeTrackChanged(index);
-		});
-	UICallbackAPI<int>::set(UICallbackType::TrackGainChanged,
-		[](int index) {
-			CoreCallbacks::getInstance()->invokeTrackGainChanged(index);
-		});
-	UICallbackAPI<int>::set(UICallbackType::TrackPanChanged,
-		[](int index) {
-			CoreCallbacks::getInstance()->invokeTrackPanChanged(index);
-		});
-	UICallbackAPI<int>::set(UICallbackType::TrackFaderChanged,
-		[](int index) {
-			CoreCallbacks::getInstance()->invokeTrackFaderChanged(index);
-		});
-	UICallbackAPI<int>::set(UICallbackType::TrackMuteChanged,
-		[](int index) {
-			CoreCallbacks::getInstance()->invokeTrackMuteChanged(index);
-		});
-	UICallbackAPI<int, int>::set(UICallbackType::EffectChanged,
-		[](int track, int index) {
-			CoreCallbacks::getInstance()->invokeEffectChanged(track, index);
-		});
-	UICallbackAPI<int>::set(UICallbackType::SeqChanged,
-		[](int index) {
-			CoreCallbacks::getInstance()->invokeSeqChanged(index);
-		});
-	UICallbackAPI<int, int>::set(UICallbackType::SeqBlockChanged,
-		[](int track, int index) {
-			CoreCallbacks::getInstance()->invokeSeqBlockChanged(track, index);
+			CoreCallbackAPI<bool>::invoke(UICallbackType::PluginSearchStateChanged, state);
 		});
 	UICallbackAPI<void>::set(UICallbackType::TempoChanged,
 		[] {
-			CoreCallbacks::getInstance()->invokeTempoChanged();
-		});
-	UICallbackAPI<int>::set(UICallbackType::SeqMuteSoloChanged,
-		[](int index) {
-			CoreCallbacks::getInstance()->invokeSeqMuteSoloChanged(index);
-		});
-	UICallbackAPI<int>::set(UICallbackType::SeqInputMonitoringChanged,
-		[](int index) {
-			CoreCallbacks::getInstance()->invokeSeqInputMonitoringChanged(index);
-		});
-	UICallbackAPI<int>::set(UICallbackType::SeqRecChanged,
-		[](int index) {
-			CoreCallbacks::getInstance()->invokeSeqRecChanged(index);
-		});
-	UICallbackAPI<int>::set(UICallbackType::SeqDataRefChanged,
-		[](int index) {
-			CoreCallbacks::getInstance()->invokeSeqDataRefChanged(index);
+			CoreCallbackAPI<void>::invoke(UICallbackType::TempoChanged);
 		});
 	UICallbackAPI<const juce::String&>::set(UICallbackType::PluginSearchMessage,
 		[](const juce::String& mes) {
-			CoreCallbacks::getInstance()->invokePluginSearchMes(mes);
+			CoreCallbackAPI<const juce::String&>::invoke(UICallbackType::PluginSearchMessage, mes);
 		});
-	UICallbackAPI<int, bool>::set(UICallbackType::SynthStateChanged,
-		[](int index, bool status) {
-			CoreCallbacks::getInstance()->invokeSynthStatus(index, status);
+
+	UICallbackAPI<void>::set(UICallbackType::GraphUpdated,
+		[] {
+			CoreCallbackAPI<void>::invoke(UICallbackType::GraphUpdated);
 		});
-	UICallbackAPI<const std::set<int>&>::set(UICallbackType::SourceRecord,
-		[](const std::set<int>& trackList) {
-			CoreCallbacks::getInstance()->invokeSourceRecord(trackList);
+	UICallbackAPI<int, int>::set(UICallbackType::TrackAdded,
+		[](int type, int index) {
+			CoreCallbackAPI<int, int>::invoke(UICallbackType::TrackAdded, type, index);
+		});
+	UICallbackAPI<int, int>::set(UICallbackType::TrackRemoved,
+		[](int type, int index) {
+			CoreCallbackAPI<int, int>::invoke(UICallbackType::TrackRemoved, type, index);
+		});
+	UICallbackAPI<int, int>::set(UICallbackType::TrackInfoChanged,
+		[](int type, int index) {
+			CoreCallbackAPI<int, int>::invoke(UICallbackType::TrackInfoChanged, type, index);
+		});
+	UICallbackAPI<int, int>::set(UICallbackType::TrackSideChainChanged,
+		[](int type, int index) {
+			CoreCallbackAPI<int, int>::invoke(UICallbackType::TrackSideChainChanged, type, index);
+		});
+	UICallbackAPI<int, int>::set(UICallbackType::TrackInputConnectionChanged,
+		[](int type, int index) {
+			CoreCallbackAPI<int, int>::invoke(UICallbackType::TrackInputConnectionChanged, type, index);
+		});
+	UICallbackAPI<int, int>::set(UICallbackType::TrackSendConnectionChanged,
+		[](int type, int index) {
+			CoreCallbackAPI<int, int>::invoke(UICallbackType::TrackSendConnectionChanged, type, index);
+		});
+	UICallbackAPI<int, int>::set(UICallbackType::TrackMuteSoloChanged,
+		[](int type, int index) {
+			CoreCallbackAPI<int, int>::invoke(UICallbackType::TrackMuteSoloChanged, type, index);
+		});
+
+	UICallbackAPI<int, int>::set(UICallbackType::TrackBlockChanged,
+		[](int track, int index) {
+			CoreCallbackAPI<int, int>::invoke(UICallbackType::TrackBlockChanged, track, index);
+		});
+	UICallbackAPI<int>::set(UICallbackType::TrackInstrChanged,
+		[](int track) {
+			CoreCallbackAPI<int>::invoke(UICallbackType::TrackInstrChanged, track);
+		});
+	UICallbackAPI<int>::set(UICallbackType::TrackDataRefChanged,
+		[](int track) {
+			CoreCallbackAPI<int>::invoke(UICallbackType::TrackDataRefChanged, track);
+		});
+	UICallbackAPI<int>::set(UICallbackType::TrackSourceChanged,
+		[](int track) {
+			CoreCallbackAPI<int>::invoke(UICallbackType::TrackSourceChanged, track);
+		});
+	UICallbackAPI<int>::set(UICallbackType::TrackRecordingChanged,
+		[](int track) {
+			CoreCallbackAPI<int>::invoke(UICallbackType::TrackRecordingChanged, track);
+		});
+	UICallbackAPI<int>::set(UICallbackType::TrackInputMonitoringChanged,
+		[](int track) {
+			CoreCallbackAPI<int>::invoke(UICallbackType::TrackInputMonitoringChanged, track);
+		});
+
+	UICallbackAPI<int, int>::set(UICallbackType::TrackGainChanged,
+		[](int type, int index) {
+			CoreCallbackAPI<int, int>::invoke(UICallbackType::TrackGainChanged, type, index);
+		});
+	UICallbackAPI<int, int>::set(UICallbackType::TrackPanChanged,
+		[](int type, int index) {
+			CoreCallbackAPI<int, int>::invoke(UICallbackType::TrackPanChanged, type, index);
+		});
+	UICallbackAPI<int, int>::set(UICallbackType::TrackFaderChanged,
+		[](int type, int index) {
+			CoreCallbackAPI<int, int>::invoke(UICallbackType::TrackFaderChanged, type, index);
+		});
+	UICallbackAPI<int, int, int>::set(UICallbackType::TrackEffectChanged,
+		[](int trackType, int trackIndex, int index) {
+			CoreCallbackAPI<int, int, int>::invoke(
+				UICallbackType::TrackEffectChanged, trackType, trackIndex, index);
 		});
 }
 
-void CoreCallbacks::addError(const ErrorCallback& callback) {
-	this->error.add(callback);
+void CoreCallbacks::addCallback(CallbackType type,
+	std::unique_ptr<CoreCallbackWrapperBase> wrapper) {
+	this->list[(int)type].add(std::move(wrapper));
 }
 
-void CoreCallbacks::addPlayingStatus(const PlayingStatusCallback& callback) {
-	this->playingStatus.add(callback);
-}
-
-void CoreCallbacks::addRecordingStatus(const RecordingStatusCallback& callback) {
-	this->recordingingStatus.add(callback);
-}
-
-void CoreCallbacks::addErrorMes(const ErrorMesCallback& callback) {
-	this->errorMes.add(callback);
-}
-
-void CoreCallbacks::addSearchPlugin(const SearchPluginCallback& callback) {
-	this->searchPlugin.add(callback);
-}
-
-void CoreCallbacks::addSourceChanged(const SourceChangedCallback& callback) {
-	this->sourceChanged.add(callback);
-}
-
-void CoreCallbacks::addInstrChanged(const InstrChangedCallback& callback) {
-	this->instrChanged.add(callback);
-}
-
-void CoreCallbacks::addTrackChanged(const TrackChangedCallback& callback) {
-	this->trackChanged.add(callback);
-}
-
-void CoreCallbacks::addTrackGainChanged(const TrackChangedCallback& callback) {
-	this->trackGainChanged.add(callback);
-}
-
-void CoreCallbacks::addTrackPanChanged(const TrackChangedCallback& callback) {
-	this->trackPanChanged.add(callback);
-}
-
-void CoreCallbacks::addTrackFaderChanged(const TrackChangedCallback& callback) {
-	this->trackFaderChanged.add(callback);
-}
-
-void CoreCallbacks::addTrackMuteChanged(const TrackChangedCallback& callback) {
-	this->trackMuteChanged.add(callback);
-}
-
-void CoreCallbacks::addEffectChanged(const EffectChangedCallback& callback) {
-	this->effectChanged.add(callback);
-}
-
-void CoreCallbacks::addSeqChanged(const SeqChangedCallback& callback) {
-	this->seqChanged.add(callback);
-}
-
-void CoreCallbacks::addSeqBlockChanged(const SeqBlockChangedCallback& callback) {
-	this->seqBlockChanged.add(callback);
-}
-
-void CoreCallbacks::addTempoChanged(const TempoChangedCallback& callback) {
-	this->tempoChanged.add(callback);
-}
-
-void CoreCallbacks::addSeqMuteSoloChanged(const SeqMuteSoloChangedCallback& callback) {
-	this->seqMuteSoloChanged.add(callback);
-}
-
-void CoreCallbacks::addSeqRecChanged(const SeqRecChangedCallback& callback) {
-	this->seqRecChanged.add(callback);
-}
-
-void CoreCallbacks::addSeqInputMonitoringChanged(const SeqInputMonitoringChangedCallback& callback) {
-	this->seqInputMonitoringChanged.add(callback);
-}
-
-void CoreCallbacks::addSeqDataRefChanged(const SeqDataRefChangedCallback& callback) {
-	this->seqDataRefChanged.add(callback);
-}
-
-void CoreCallbacks::addPluginSearchMes(const PluginSearchMesCallback& callback) {
-	this->pluginSearchMesChanged.add(callback);
-}
-
-void CoreCallbacks::addSynthStatus(const SynthStatusCallback& callback) {
-	this->synthStatus.add(callback);
-}
-
-void CoreCallbacks::addSourceRecord(const SourceRecordCallback& callback) {
-	this->sourceRecord.add(callback);
-}
-
-void CoreCallbacks::addEditingSeqChanged(const EditingSeqChangedCallback& callback) {
-	this->editingSeqChanged.add(callback);
-}
-
-void CoreCallbacks::invokeError(
-	const juce::String& title, const juce::String& mes) const {
-	for (auto& i : this->error) {
-		i(title, mes);
-	}
-}
-
-void CoreCallbacks::invokePlayingStatus(bool status) const {
-	for (auto& i : this->playingStatus) {
-		i(status);
-	}
-}
-
-void CoreCallbacks::invokeRecordingStatus(bool status) const {
-	for (auto& i : this->recordingingStatus) {
-		i(status);
-	}
-}
-
-void CoreCallbacks::invokeErrorMes(const juce::String& mes) const {
-	for (auto& i : this->errorMes) {
-		i(mes);
-	}
-}
-
-void CoreCallbacks::invokeSearchPlugin(bool status) const {
-	for (auto& i : this->searchPlugin) {
-		i(status);
-	}
-}
-
-void CoreCallbacks::invokeSourceChanged(int index) const {
-	for (auto& i : this->sourceChanged) {
-		i(index);
-	}
-}
-
-void CoreCallbacks::invokeInstrChanged(int index) const {
-	for (auto& i : this->instrChanged) {
-		i(index);
-	}
-}
-
-void CoreCallbacks::invokeTrackChanged(int index) const {
-	for (auto& i : this->trackChanged) {
-		i(index);
-	}
-}
-
-void CoreCallbacks::invokeTrackGainChanged(int index) const {
-	for (auto& i : this->trackGainChanged) {
-		i(index);
-	}
-}
-
-void CoreCallbacks::invokeTrackPanChanged(int index) const {
-	for (auto& i : this->trackPanChanged) {
-		i(index);
-	}
-}
-
-void CoreCallbacks::invokeTrackFaderChanged(int index) const {
-	for (auto& i : this->trackFaderChanged) {
-		i(index);
-	}
-}
-
-void CoreCallbacks::invokeTrackMuteChanged(int index) const {
-	for (auto& i : this->trackMuteChanged) {
-		i(index);
-	}
-}
-
-void CoreCallbacks::invokeEffectChanged(int track, int index) const {
-	for (auto& i : this->effectChanged) {
-		i(track, index);
-	}
-}
-
-void CoreCallbacks::invokeSeqChanged(int index) const {
-	for (auto& i : this->seqChanged) {
-		i(index);
-	}
-}
-
-void CoreCallbacks::invokeSeqBlockChanged(int track, int index) const {
-	for (auto& i : this->seqBlockChanged) {
-		i(track, index);
-	}
-}
-
-void CoreCallbacks::invokeTempoChanged() const {
-	for (auto& i : this->tempoChanged) {
-		i();
-	}
-}
-
-void CoreCallbacks::invokeSeqMuteSoloChanged(int index) const {
-	for (auto& i : this->seqMuteSoloChanged) {
-		i(index);
-	}
-}
-
-void CoreCallbacks::invokeSeqRecChanged(int index) const {
-	for (auto& i : this->seqRecChanged) {
-		i(index);
-	}
-}
-
-void CoreCallbacks::invokeSeqInputMonitoringChanged(int index) const {
-	for (auto& i : this->seqInputMonitoringChanged) {
-		i(index);
-	}
-}
-
-void CoreCallbacks::invokeSeqDataRefChanged(int index) const {
-	for (auto& i : this->seqDataRefChanged) {
-		i(index);
-	}
-}
-
-void CoreCallbacks::invokePluginSearchMes(const juce::String& mes) const {
-	for (auto& i : this->pluginSearchMesChanged) {
-		i(mes);
-	}
-}
-
-void CoreCallbacks::invokeSynthStatus(int index, bool status) const {
-	for (auto& i : this->synthStatus) {
-		i(index, status);
-	}
-}
-
-void CoreCallbacks::invokeSourceRecord(const std::set<int>& trackList) const {
-	for (auto& i : this->sourceRecord) {
-		i(trackList);
-	}
-}
-
-void CoreCallbacks::invokeEditingSeqChanged(int index) const {
-	for (auto& i : this->editingSeqChanged) {
-		i(index);
-	}
+const juce::OwnedArray<CoreCallbackWrapperBase>& CoreCallbacks::getCallbackList(CallbackType type) const {
+	return this->list[(int)type];
 }
 
 CoreCallbacks* CoreCallbacks::getInstance() {
--- src/ui/misc/CoreCallbacks.h
+++ src/ui/misc/CoreCallbacks.h
@@ -1,106 +1,94 @@
 ﻿#pragma once
 
 #include <JuceHeader.h>
+#include "../../audioCore/AC_API.h"
+
+class CoreCallbackWrapperBase {
+public:
+	virtual ~CoreCallbackWrapperBase() = default;
+
+protected:
+	CoreCallbackWrapperBase(const std::type_info& type)
+		: type(type) {
+	};
+
+	bool isType(const std::type_info& type) const {
+		return this->type == type;
+	};
+
+private:
+	const std::type_info& type;
+
+	JUCE_DECLARE_NON_COPYABLE(CoreCallbackWrapperBase)
+	JUCE_LEAK_DETECTOR(CoreCallbackWrapperBase)
+};
+
+template<typename... T>
+class CoreCallbackWrapper final : public CoreCallbackWrapperBase {
+public:
+	using Func = std::function<void(T...)>;
+
+	CoreCallbackWrapper(const Func& func)
+		: CoreCallbackWrapperBase(typeid(Func)), func(func) {
+	};
+
+	void invoke(T... args) const {
+		/** Type Mismatch */
+		if (!this->isType(typeid(Func))) {
+			jassertfalse;
+			return;
+		}
+
+		/** Invoke Function */
+		this->func(args...);
+	}
+
+private:
+	const Func func;
+
+	JUCE_DECLARE_NON_COPYABLE(CoreCallbackWrapper)
+	JUCE_LEAK_DETECTOR(CoreCallbackWrapper<T...>)
+};
+
+template<>
+class CoreCallbackWrapper<void> final : public CoreCallbackWrapperBase {
+public:
+	using Func = std::function<void(void)>;
+
+	CoreCallbackWrapper(const Func& func)
+		: CoreCallbackWrapperBase(typeid(Func)), func(func) {
+	};
+
+	void invoke() const {
+		/** Type Mismatch */
+		if (!this->isType(typeid(Func))) {
+			jassertfalse;
+			return;
+		}
+
+		/** Invoke Function */
+		this->func();
+	}
+
+private:
+	const Func func;
+
+	JUCE_DECLARE_NON_COPYABLE(CoreCallbackWrapper)
+	JUCE_LEAK_DETECTOR(CoreCallbackWrapper<void>)
+};
 
 class CoreCallbacks final : private juce::DeletedAtShutdown {
 public:
 	CoreCallbacks();
 
-	using ErrorCallback = std::function<void(const juce::String&, const juce::String&)>;
-	void addError(const ErrorCallback& callback);
-	using PlayingStatusCallback = std::function<void(bool)>;
-	void addPlayingStatus(const PlayingStatusCallback& callback);
-	using RecordingStatusCallback = std::function<void(bool)>;
-	void addRecordingStatus(const RecordingStatusCallback& callback);
-	using ErrorMesCallback = std::function<void(const juce::String&)>;
-	void addErrorMes(const ErrorMesCallback& callback);
-	using SearchPluginCallback = std::function<void(bool)>;
-	void addSearchPlugin(const SearchPluginCallback& callback);
-	using SourceChangedCallback = std::function<void(int)>;
-	void addSourceChanged(const SourceChangedCallback& callback);
-	using InstrChangedCallback = std::function<void(int)>;
-	void addInstrChanged(const InstrChangedCallback& callback);
-	using TrackChangedCallback = std::function<void(int)>;
-	void addTrackChanged(const TrackChangedCallback& callback);
-	void addTrackGainChanged(const TrackChangedCallback& callback);
-	void addTrackPanChanged(const TrackChangedCallback& callback);
-	void addTrackFaderChanged(const TrackChangedCallback& callback);
-	void addTrackMuteChanged(const TrackChangedCallback& callback);
-	using EffectChangedCallback = std::function<void(int, int)>;
-	void addEffectChanged(const EffectChangedCallback& callback);
-	using SeqChangedCallback = std::function<void(int)>;
-	void addSeqChanged(const SeqChangedCallback& callback);
-	using SeqBlockChangedCallback = std::function<void(int, int)>;
-	void addSeqBlockChanged(const SeqBlockChangedCallback& callback);
-	using TempoChangedCallback = std::function<void(void)>;
-	void addTempoChanged(const TempoChangedCallback& callback);
-	using SeqMuteSoloChangedCallback = std::function<void(int)>;
-	void addSeqMuteSoloChanged(const SeqMuteSoloChangedCallback& callback);
-	using SeqRecChangedCallback = std::function<void(int)>;
-	void addSeqRecChanged(const SeqRecChangedCallback& callback);
-	using SeqInputMonitoringChangedCallback = std::function<void(int)>;
-	void addSeqInputMonitoringChanged(const SeqInputMonitoringChangedCallback& callback);
-	using SeqDataRefChangedCallback = std::function<void(int)>;
-	void addSeqDataRefChanged(const SeqDataRefChangedCallback& callback);
-	using PluginSearchMesCallback = std::function<void(const juce::String&)>;
-	void addPluginSearchMes(const PluginSearchMesCallback& callback);
-	using SynthStatusCallback = std::function<void(int, bool)>;
-	void addSynthStatus(const SynthStatusCallback& callback);
-	using SourceRecordCallback = std::function<void(const std::set<int>&)>;
-	void addSourceRecord(const SourceRecordCallback& callback);
-	using EditingSeqChangedCallback = std::function<void(int)>;
-	void addEditingSeqChanged(const EditingSeqChangedCallback& callback);
+	using CallbackType = UICallbackType;
 
-	void invokeError(const juce::String& title, const juce::String& mes) const;
-	void invokePlayingStatus(bool status) const;
-	void invokeRecordingStatus(bool status) const;
-	void invokeErrorMes(const juce::String& mes) const;
-	void invokeSearchPlugin(bool status) const;
-	void invokeSourceChanged(int index) const;
-	void invokeInstrChanged(int index) const;
-	void invokeTrackChanged(int index) const;
-	void invokeTrackGainChanged(int index) const;
-	void invokeTrackPanChanged(int index) const;
-	void invokeTrackFaderChanged(int index) const;
-	void invokeTrackMuteChanged(int index) const;
-	void invokeEffectChanged(int track, int index) const;
-	void invokeSeqChanged(int index) const;
-	void invokeSeqBlockChanged(int track, int index) const;
-	void invokeTempoChanged() const;
-	void invokeSeqMuteSoloChanged(int index) const;
-	void invokeSeqRecChanged(int index) const;
-	void invokeSeqInputMonitoringChanged(int index) const;
-	void invokeSeqDataRefChanged(int index) const;
-	void invokePluginSearchMes(const juce::String& mes) const;
-	void invokeSynthStatus(int index, bool status) const;
-	void invokeSourceRecord(const std::set<int>& trackList) const;
-	void invokeEditingSeqChanged(int index) const;
+	void addCallback(CallbackType type,
+		std::unique_ptr<CoreCallbackWrapperBase> wrapper);
+	const juce::OwnedArray<CoreCallbackWrapperBase>& getCallbackList(CallbackType type) const;
 
 private:
-	juce::Array<ErrorCallback> error;
-	juce::Array<PlayingStatusCallback> playingStatus;
-	juce::Array<RecordingStatusCallback> recordingingStatus;
-	juce::Array<ErrorMesCallback> errorMes;
-	juce::Array<SearchPluginCallback> searchPlugin;
-	juce::Array<SourceChangedCallback> sourceChanged;
-	juce::Array<InstrChangedCallback> instrChanged;
-	juce::Array<TrackChangedCallback> trackChanged;
-	juce::Array<TrackChangedCallback> trackGainChanged;
-	juce::Array<TrackChangedCallback> trackPanChanged;
-	juce::Array<TrackChangedCallback> trackFaderChanged;
-	juce::Array<TrackChangedCallback> trackMuteChanged;
-	juce::Array<EffectChangedCallback> effectChanged;
-	juce::Array<SeqChangedCallback> seqChanged;
-	juce::Array<SeqBlockChangedCallback> seqBlockChanged;
-	juce::Array<TempoChangedCallback> tempoChanged;
-	juce::Array<SeqMuteSoloChangedCallback> seqMuteSoloChanged;
-	juce::Array<SeqInputMonitoringChangedCallback> seqInputMonitoringChanged;
-	juce::Array<SeqRecChangedCallback> seqRecChanged;
-	juce::Array<SeqDataRefChangedCallback> seqDataRefChanged;
-	juce::Array<PluginSearchMesCallback> pluginSearchMesChanged;
-	juce::Array<SynthStatusCallback> synthStatus;
-	juce::Array<SourceRecordCallback> sourceRecord;
-	juce::Array<EditingSeqChangedCallback> editingSeqChanged;
+	std::array<juce::OwnedArray<CoreCallbackWrapperBase>, (int)CallbackType::TypeMaxNum> list;
 
 public:
 	static CoreCallbacks* getInstance();
@@ -110,4 +98,46 @@ private:
 	static CoreCallbacks* instance;
 
 	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(CoreCallbacks)
+};
+
+template<typename... T>
+class CoreCallbackAPI final {
+	CoreCallbackAPI() = delete;
+
+public:
+	static void invoke(CoreCallbacks::CallbackType type, T... args) {
+		auto& list = CoreCallbacks::getInstance()->getCallbackList(type);
+		for (auto i : list) {
+			if (auto wrapper = dynamic_cast<CoreCallbackWrapper<T...>*>(i)) {
+				wrapper->invoke(args...);
+			}
+		}
+	};
+
+	static void add(CoreCallbacks::CallbackType type, const std::function<void(T...)>& func) {
+		auto wrapper = std::make_unique<CoreCallbackWrapper<T...>>(func);
+		CoreCallbacks::getInstance()->addCallback(
+			type, std::unique_ptr<CoreCallbackWrapperBase>(wrapper.release()));
+	};
+};
+
+template<>
+class CoreCallbackAPI<void> final {
+	CoreCallbackAPI() = delete;
+
+public:
+	static void invoke(UICallbackType type) {
+		auto& list = CoreCallbacks::getInstance()->getCallbackList(type);
+		for (auto i : list) {
+			if (auto wrapper = dynamic_cast<CoreCallbackWrapper<void>*>(i)) {
+				wrapper->invoke();
+			}
+		}
+	};
+
+	static void add(CoreCallbacks::CallbackType type, const std::function<void(void)>& func) {
+		auto wrapper = std::make_unique<CoreCallbackWrapper<void>>(func);
+		CoreCallbacks::getInstance()->addCallback(
+			type, std::unique_ptr<CoreCallbackWrapperBase>(wrapper.release()));
+	};
 };
