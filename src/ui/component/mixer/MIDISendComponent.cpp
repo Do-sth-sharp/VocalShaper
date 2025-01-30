23f54320c83658f1e86af4adfc7891a30f3cf5bf
無常 <3142324836@qq.com>
2025-01-30 16:52:02 +0800

    Fixed aux track send to self

--- src/ui/component/mixer/MIDISendComponent.cpp
+++ src/ui/component/mixer/MIDISendComponent.cpp
@@ -246,7 +246,8 @@ juce::PopupMenu MIDISendComponent::createAddMenu(
 			name = TRANS("AUX Track") + " #" + juce::String{ i };
 		}
 		menu.addItem(name,
-			this->type != (int)quickAPI::TrackType::MasterTrack,
+			this->type != (int)quickAPI::TrackType::MasterTrack
+			&& !(this->type == (int)quickAPI::TrackType::AuxTrack && this->track == i),
 			dst.second == i && dst.first == quickAPI::SendDstType::ToAUX,
 			std::bind(addCallback, (int)quickAPI::SendDstType::ToAUX, i));
 	}
