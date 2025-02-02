79f79b8eb26e4d077df2814758cfb913d1a566a5
無常 <3142324836@qq.com>
2025-02-02 15:39:59 +0800

    Update linux CI

--- scripts/ci/findBinary.ps1
+++ scripts/ci/findBinary.ps1
@@ -7,16 +7,22 @@ if (-not (Test-Path -Path $DirectoryPath)) {
     exit
 }
 
-# Get all .exe .dll and .vst3 files recursively from the specified directory
-$files = Get-ChildItem -Path $DirectoryPath -Recurse -Include *.exe, *.dll, *.vst3
+if ($IsWindows) {
+    # Get all .exe .dll and .vst3 files recursively from the specified directory
+    $files = Get-ChildItem -Path $DirectoryPath -Recurse -Include *.exe, *.dll, *.vst3
+    
+    # Format the output
+    $output = $files | ForEach-Object {
+        '"' + $_.FullName + '"'
+    } 
 
-# Format the output
-$output = $files | ForEach-Object {
-    '"' + $_.FullName + '"'
-} 
-
-# Join the file names with a comma separator
-$joinedOutput = $output -join ","
+    # Join the file names with a comma separator
+    $joinedOutput = $output -join ","
+}
+else {
+    # Get all ELF Mach-O and universal binary files recursively from the specified directory
+    $joinedOutput = & ./findBinary-unix.sh $DirectoryPath
+}
 
 # Print the output
 Write-Output $joinedOutput
