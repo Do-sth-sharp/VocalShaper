4ca786b5b50201d84aa107e3932117d627761f3d
無常 <3142324836@qq.com>
2025-02-14 00:36:13 +0800

    Add MIDI note time edit action

--- src/audioCore/action/ActionMIDI.cpp
+++ src/audioCore/action/ActionMIDI.cpp
@@ -42,3 +42,57 @@ void ActionMIDIAddNote::getRecoveryData(juce::MemoryOutputStream& stream) {
 	stream.writeByte((char)this->vel);
 	stream.writeString(this->lyrics);
 }
+
+ActionMIDISetNoteTime::ActionMIDISetNoteTime(
+	uint64_t ref, int track, int index,
+	double startTime, double endTime)
+	: ref(ref), track(track), index(index),
+	startTime(startTime), endTime(endTime) {}
+
+bool ActionMIDISetNoteTime::doAction() {
+	/** Get Old Index */
+	auto note = SourceManager::getInstance()->getMIDINote(
+		this->ref, this->track, this->index);
+	if (note.eventOffIndex < 0) { return false; }
+	this->oldEndIndex = note.eventOffIndex;
+	this->oldStartTime = note.timeSec;
+	this->oldEndTime = note.endSec;
+
+	/** Set Time */
+	this->newIndex = SourceManager::getInstance()->setNoteTime(
+		this->ref, this->track, this->index,
+		this->startTime, this->endTime);
+	if (this->newIndex < 0) { return false; }
+
+	return true;
+}
+
+bool ActionMIDISetNoteTime::undoAction() {
+	int index = SourceManager::getInstance()->setNoteTime(
+		this->ref, this->track, this->newIndex,
+		this->oldStartTime, this->oldEndTime,
+		this->index, this->oldEndIndex);
+	return (index == this->index);
+}
+
+const juce::String ActionMIDISetNoteTime::getStatusStr() const {
+	return "Source: " + juce::String{ this->ref } + "\n" +
+		"Track: " + juce::String{ this->track } + "\n" +
+		"Index: " + juce::String{ this->index } + "\n" +
+		"Old Time: " + juce::String{ this->oldStartTime, 2 } + " - " + juce::String{ this->oldEndTime, 2 } + "\n" +
+		"Time: " + juce::String{ this->startTime, 2 } + " - " + juce::String{ this->endTime, 2 } + "\n" +
+		"New Index: " + juce::String{ this->newIndex } + "\n";
+}
+
+void ActionMIDISetNoteTime::getRecoveryData(juce::MemoryOutputStream& stream) {
+	stream.writeInt64((int64_t)this->ref);
+	stream.writeInt(this->track);
+	stream.writeInt(this->index);
+	stream.writeDouble(this->startTime);
+	stream.writeDouble(this->endTime);
+
+	stream.writeInt(this->oldEndIndex);
+	stream.writeInt(this->newIndex);
+	stream.writeDouble(this->oldStartTime);
+	stream.writeDouble(this->oldEndTime);
+}
--- src/audioCore/action/ActionMIDI.h
+++ src/audioCore/action/ActionMIDI.h
@@ -33,3 +33,32 @@ private:
 
 	JUCE_LEAK_DETECTOR(ActionMIDIAddNote)
 };
+
+class ActionMIDISetNoteTime final : public ActionUndoableBase {
+public:
+	ActionMIDISetNoteTime() = delete;
+	ActionMIDISetNoteTime(
+		uint64_t ref, int track, int index,
+		double startTime, double endTime);
+
+	bool doAction() override;
+	bool undoAction() override;
+	const juce::String getName() const override {
+		return "MIDI Set Note Time";
+	};
+	ActionType getActionType() const override { return ActionType::ActionMIDISetNoteTime; };
+	const juce::String getStatusStr() const override;
+	void getRecoveryData(juce::MemoryOutputStream& stream) override;
+
+private:
+	const uint64_t ref;
+	const int track, index;
+	const double startTime;
+	const double endTime;
+
+	int oldEndIndex = -1;
+	int newIndex = -1;
+	double oldStartTime = 0, oldEndTime = 0;
+
+	JUCE_LEAK_DETECTOR(ActionMIDISetNoteTime)
+};
